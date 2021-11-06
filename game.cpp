eef93076612dd1386149139ad1d2e9f0a86ef147
JacksonMHoke <jacksonmhoke@gmail.com>
2021-11-06 11:20:49 -0700

    Added skeleton functions for game

---
+++ game.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
+
+using namespace std;
+
+pair<int, int> getCoordinates() {
+
+}
+
+void display() {
+
+}
+
+void populate(vector<vector<int>>& grid, vector<int>& hps) {
+
+}
+
+void turn (vector<vector<int>>& opposingGrid, vector<int>& hps) {
+
+}
+
+bool hasWon(int player, vector<int>& hps) {
+
+}
+
+void run() {
+
+}
